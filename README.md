e79cd985bbed8c5b5ce676ff45c49bd716654e2c
Joe O'Regan <joeaoregan@yahoo.ie>
2019-01-10 14:10:49 +0000

    Tutorial Part 1

---
+++ README.md
@@ -0,0 +1,6 @@
+# Multiplayer Browser Game
+
+Multiplayer browser game using JavaScript and Node.js
+
+## Links
+1. [HackerNoon: How to build a Multiplayer Browser Game (Part 1)](https://hackernoon.com/how-to-build-a-multiplayer-browser-game-4a793818c29b)
---
+++ static/game.js
@@ -0,0 +1,69 @@
+var socket = io();
+socket.on('message', function(data) {
+  console.log(data);
+});
+
+// Keyboard states
+var movement = {
+		  up: false,
+		  down: false,
+		  left: false,
+		  right: false
+		}
+		document.addEventListener('keydown', function(event) {
+		  switch (event.keyCode) {
+		    case 65: // A
+		      movement.left = true;
+		      break;
+		    case 87: // W
+		      movement.up = true;
+		      break;
+		    case 68: // D
+		      movement.right = true;
+		      break;
+		    case 83: // S
+		      movement.down = true;
+		      break;
+		  }
+		});
+
+document.addEventListener('keyup', function(event) {
+	  switch (event.keyCode) {
+	    case 65: // A
+	      movement.left = false;
+	      break;
+	    case 87: // W
+	      movement.up = false;
+	      break;
+	    case 68: // D
+	      movement.right = false;
+	      break;
+	    case 83: // S
+	      movement.down = false;
+	      break;
+	  }
+	});
+		
+// Alert server that a new player has joined
+socket.emit('new player');
+setInterval(function() {
+  socket.emit('movement', movement);
+}, 1000 / 60);
+		
+		
+// Handler to draw data on the Server to the HTML5 canvas
+// Access canvas and draw to it
+var canvas = document.getElementById('canvas');
+canvas.width = 800;
+canvas.height = 600;
+var context = canvas.getContext('2d');
+socket.on('state', function(players) {
+  context.clearRect(0, 0, 800, 600);
+  context.fillStyle = 'green';
+  for (var id in players) {
+    var player = players[id];
+    context.beginPath();
+    context.arc(player.x, player.y, 10, 0, 2 * Math.PI);
+    context.fill();
+  }
+});
