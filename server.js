66d219fbf4a345a3ac9a562d5728bcb5e90111df
Joe O'Regan <joeaoregan@yahoo.ie>
2019-01-11 21:11:49 +0000

    update controls, connect & disconnect message

--- server.js
+++ server.js
@@ -21,10 +21,6 @@ server.listen(5000, function() {
   console.log('Starting server on port 5000');
 });
 
-//Add the WebSocket handlers
-io.on('connection', function(socket) {
-});
-
 //// For testing
 //setInterval(function() {
 //	  io.sockets.emit('message', 'hi!');
@@ -32,29 +28,36 @@ io.on('connection', function(socket) {
 
 // Handle Client Keyboard State 60 times a second
 var players = {};
-io.on('connection', function(socket) {
-  socket.on('new player', function() {
-    players[socket.id] = {
-      x: 300,
-      y: 300
-    };
-  });
-  socket.on('movement', function(data) {
-    var player = players[socket.id] || {};
-    if (data.left) {
-      player.x -= 5;
-    }
-    if (data.up) {
-      player.y -= 5;
-    }
-    if (data.right) {
-      player.x += 5;
-    }
-    if (data.down) {
-      player.y += 5;
-    }
-  });
+io.on('connection', function(socket) {	
+	socket.on('new player', function() {
+		console.log("New player connected. Socket ID: %s", socket.id);
+		players[socket.id] = {
+			x: 300,
+			y: 300
+		};
+	});
+
+	socket.on('movement', function(data) {
+		var player = players[socket.id] || {};
+		
+		if (data.left) {
+			player.x -= 5;
+		} else if (data.right) {
+			player.x += 5;
+		}
+		
+		if (data.up) {
+			player.y -= 5;
+		} else if (data.down) {
+			player.y += 5;
+		}
+	});
+	
+	socket.on('disconnect', function() {
+		console.log('Player with socket ID: %s has disconnected', socket.id);	
+	});
 });
+
 setInterval(function() {
   io.sockets.emit('state', players);
 }, 1000 / 60);
--- static/game.js
+++ static/game.js
@@ -5,49 +5,67 @@ socket.on('message', function(data) {
 
 // Keyboard states
 var movement = {
-		  up: false,
-		  down: false,
-		  left: false,
-		  right: false
-		}
-		document.addEventListener('keydown', function(event) {
-		  switch (event.keyCode) {
-		    case 65: // A
-		      movement.left = true;
-		      break;
-		    case 87: // W
-		      movement.up = true;
-		      break;
-		    case 68: // D
-		      movement.right = true;
-		      break;
-		    case 83: // S
-		      movement.down = true;
-		      break;
-		  }
-		});
+	up: false,
+	down: false,
+	left: false,
+	right: false
+}
+				
+document.addEventListener('keydown', function(event) {
+	switch (event.keyCode) {
+		case 65: // A
+		case 37: // Left
+		case 100: // 4
+			movement.left = true;
+			break;
+		case 87: // W
+		case 38: // Up
+		case 104: // 8
+			movement.up = true;
+			break;
+		case 68: // D
+		case 39: // Right
+		case 102: // 6
+			movement.right = true;
+			break;
+		case 83: // S
+		case 40: // Down
+		case 98: // 2
+			movement.down = true;
+			break;
+	}
+});
 
 document.addEventListener('keyup', function(event) {
-	  switch (event.keyCode) {
-	    case 65: // A
-	      movement.left = false;
-	      break;
-	    case 87: // W
-	      movement.up = false;
-	      break;
-	    case 68: // D
-	      movement.right = false;
-	      break;
-	    case 83: // S
-	      movement.down = false;
-	      break;
-	  }
-	});
+	switch (event.keyCode) {
+		case 65: // A
+		case 37: // Left
+		case 100: // 4
+			movement.left = false;
+			break;
+		case 87: // W
+		case 38: // Up
+		case 104: // 8
+			movement.up = false;
+			break;
+		case 68: // D
+		case 39: // Right
+		case 102: // 6
+			movement.right = false;
+			break;
+		case 83: // S
+		case 40: // Down
+		case 98: // 2
+			movement.down = false;
+			break;
+	}
+});
 		
 // Alert server that a new player has joined
 socket.emit('new player');
+
 setInterval(function() {
-  socket.emit('movement', movement);
+	socket.emit('movement', movement);
 }, 1000 / 60);
 		
 		
@@ -56,14 +74,16 @@ setInterval(function() {
 var canvas = document.getElementById('canvas');
 canvas.width = 800;
 canvas.height = 600;
+
 var context = canvas.getContext('2d');
 socket.on('state', function(players) {
-  context.clearRect(0, 0, 800, 600);
-  context.fillStyle = 'green';
-  for (var id in players) {
-    var player = players[id];
-    context.beginPath();
-    context.arc(player.x, player.y, 10, 0, 2 * Math.PI);
-    context.fill();
-  }
+	context.clearRect(0, 0, 800, 600);
+	context.fillStyle = 'green';
+	
+	for (var id in players) {
+		var player = players[id];
+		context.beginPath();
+		context.arc(player.x, player.y, 10, 0, 2 * Math.PI);
+		context.fill();
+	}
 });
