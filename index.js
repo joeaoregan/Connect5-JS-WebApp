db7f6e3b86a9001b95d8ea24bbb52381a7224b8d
Joe O'Regan <joeaoregan@yahoo.ie>
2019-01-16 15:45:29 +0000

    Move game logic to server

--- index.js
+++ index.js
@@ -2,35 +2,34 @@
 	Joe O'Regan
 	index.js
 */
-var express = require('express');
-var http = require('http');
-var path = require('path');
-var socketIO = require('socket.io');
-var port = process.env.PORT || 1337;
-var app = express();
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const socketIO = require('socket.io');
+const port = process.env.PORT || 1337;
+const app = express();
+const server = http.Server(app).listen(port);
+const io = socketIO(server);
 
-var server = http.Server(app, function(req, res) {
-    console.log(`${req.method} request for ${req.url}`);
-	
-	if (req.url === "/") {
-		fs.readFile("./index.html", "UTF-8", function(err, html) {
-			res.writeHead(200, {"Content-Type": "text/html"});
-			res.end(html);
-		});
-	} else if (req.url.match(/.css$/)) {
-		var cssPath = path.join(__dirname, req.url);		
-		var fileStream = fs.createReadStream(cssPath, "UTF-8");
-		
-		res.writeHead(200, {"Content-Type": "text/css"});
-		
-		fileStream.pipe(res);	// stream contents of file to response		
-	} else {
-		res.writeHead(404, {"Content-Type": "text/plain"});
-		res.end("404 File Not Found");
-	}
-}).listen(port);
+const CONNECT = 5;
+const ROWS = 6;
+const COLS = 9;
+const PLAYER_1 = 1;
+const PLAYER_2 = 2;
 
-var io = socketIO(server);
+var games = 0;
+var currentPlayer = PLAYER_1;
+var fiveInARow;
+var ready;
+var gameOver;
+
+var board = [[0,0,0,0,0,0,0,0,0],	// init board
+			 [0,0,0,0,0,0,0,0,0],
+			 [0,0,0,0,0,0,0,0,0],
+			 [0,0,0,0,0,0,0,0,0],
+			 [0,0,0,0,0,0,0,0,0],
+			 [0,0,0,0,0,0,0,0,0]];			
+var winBoard = board;	
 
 console.log("Server running at http://localhost:%d", port);
 
@@ -53,4 +52,203 @@ app.get('/favicon.ico', function(req, res) {
 
 io.on('connection', (socket) => {
 	console.log("New Connection");
+	console.log("New Connection");
+		
+    // Create a new game and notify player
+    socket.on('create', (data) => {
+        socket.join(`game-${++games}`);	// subscribe to a specific channel (game), join a room
+        socket.emit('newGame', { username: data.username, gameID: `game-${games}` });
+		console.log('Game-'+games+' created by player 1: '+data.username+'. Waiting on Player 2');
+    });
+	
+	socket.on('col', (data) => {		
+		if (data.player === currentPlayer) {
+			checkCol(data.column);
+			checkWin(data.player);
+		
+			// sockets join a room, makes it easier to to broadcast messages to other sockets
+			//socket.broadcast.to(data.gameID).emit('turnPlayed', {
+			//io.to(data.gameID).emit('event');	// data.gameID = room
+			socket.emit('turnPlayed', {
+				board: board,
+				column: data.column,
+				gameID: data.gameID,
+				player: currentPlayer
+			});
+			
+			if (!gameOver) {
+				changePlayer();	// Move complete, change the active player
+			}
+			
+			console.log("column "+data.column+" selected by player " + data.player);
+		} else {
+			console.log("Not Your Turn Player %s!", data.player);
+		}	
+	});
+	
+    // Connect the Player 2 to the room he requested. Show error if room full.
+    socket.on('player2join', function (data) {
+		console.log("player2join: " + data.username + " has joined " + data.gameID);
+		
+		currentPlayer = PLAYER_1;
+		
+        var game = io.nsps['/'].adapter.rooms[data.gameID];
+		
+        if (game && game.length === 1) {
+            socket.join(data.gameID);			
+            socket.broadcast.to(data.gameID).emit('player1', {});			
+            socket.emit('player2', { username: data.username, gameID: data.gameID })			
+        } else {
+            socket.emit('err', { message: 'This game is already full' });
+        }
+    });
+
+    // Broadcast game winner
+    socket.on('gameOver', (data) => {
+        socket.broadcast.to(data.gameID).emit('gameEnd', data);
+    });
 });
+
+
+
+// Game
+
+function checkCol(col) {
+	console.log('Checking column for ' + currentPlayer);
+	if (board[0][col] != 0) {
+		console.log('\x1b[31mError:\x1b[0m Column %s is full!', col);
+	} else {	
+		for (var i = ROWS-1; i >= 0; i--) {
+			if (board[i][col] == 0) {
+				board[i][col] = currentPlayer;
+				break;
+			}
+		}	
+		
+		//checkWin(currentPlayer);
+		
+		//if (!gameOver) {
+		//	changePlayer();	// Move complete, change the active player
+		//}
+		displayBoard(board);
+	}
+}
+
+
+function checkWin(player) {
+	console.log("Checking for winner");
+	// Diagonals
+	var win = false;
+	
+	for (var row = 0; row <= ROWS - CONNECT; row++) {
+		// up to right
+		for (var col = CONNECT-1; col < COLS; col++) {
+			if (board[row][col] == player && board[row + 1][col - 1] == player && board[row + 2][col - 2] == player
+					&& board[row + 3][col - 3] == player && board[row + 4][col - 4] == player) {
+				win = true;
+				fiveInARow = [row,col,row+1,col-1,row+2,col-2,row+3,col-3,row+4,col-4]; // Highlight winning move
+				break;
+			}
+		}
+		
+		// up to left
+		if (!win) {
+			for (var col = 0; col <= COLS - CONNECT; col++) {
+				if (board[row][col] == player && board[row + 1][col + 1] == player && board[row + 2][col + 2] == player
+						&& board[row + 3][col + 3] == player && board[row + 4][col + 4] == player) {
+					win = true;
+					fiveInARow = [row,col,row+1,col+1,row+2,col+2,row+3,col+3,row+4,col+4];
+					break;
+				}
+			}
+		} else {
+			break;
+		}
+	}
+
+	// Check Rows
+	for (var row = 0; row < ROWS; row++) {
+		for (var col = 0; col <= COLS - CONNECT; col++) {
+			if (board[row][col] == player && board[row][col + 1] == player && board[row][col + 2] == player
+					&& board[row][col + 3] == player && board[row][col + 4] == player) {
+				win = true;
+				fiveInARow = [row,col,row,col+1,row,col+2,row,col+3,row,col+4];
+				break;
+			}
+		}
+		if (win) break; // no need for further checks
+	}
+
+	// Check Columns
+	for (var row = 0; row <= ROWS - CONNECT; row++) {
+		for (var col = 0; col < COLS; col++) {
+			if (board[row][col] == player && board[row + 1][col] == player && board[row + 2][col] == player
+					&& board[row + 3][col] == player && board[row + 4][col] == player) {
+				win = true;
+				fiveInARow = [row,col,row+1,col,row+2,col,row+3,col,row+4,col];
+				break;
+			}
+		}
+		if (win) break; 
+	}
+	
+	if (win) {
+		gameOver = true;
+		console.log('\x1b[32mPlayer %d is the winner!', player);
+		show5InARow();
+	}
+}
+
+function displayBoard(board) {
+	console.log();
+	console.log('  \x1b[36m%s\x1b[0m %s', 'Connect5', 'by Joe O\'Regan');  //cyan
+
+	var redO = "\x1b[31mO";
+	var yellowO = "\x1b[33mO";
+	var emptyCol = "\x1b[34m ";
+	var greenO = "\x1b[32mO";
+
+	console.log('\x1b[34m_\x1b[0m1\x1b[34m__\x1b[0m2\x1b[34m__\x1b[0m3\x1b[34m__\x1b[0m4\x1b[34m__\x1b[0m5\x1b[34m__\x1b[0m6\x1b[34m__\x1b[0m7\x1b[34m__\x1b[0m8\x1b[34m__\x1b[0m9\x1b[34m_');
+	for (var i = 0; i < ROWS; i++) { // display 0 to 5
+		console.log('\x1b[34m[%s\x1b[34m][\x1b[0m%s\x1b[34m][%s\x1b[34m][%s\x1b[34m][%s\x1b[34m][%s\x1b[34m][%s\x1b[34m][%s\x1b[34m][%s\x1b[34m]',
+		(board[i][0] === 0) ? emptyCol : (board[i][0] == 1) ? redO : (board[i][0] == 2) ? yellowO : greenO,
+		(board[i][1] === 0) ? emptyCol : (board[i][1] == 1) ? redO : (board[i][1] == 2) ? yellowO : greenO,
+		(board[i][2] === 0) ? emptyCol : (board[i][2] == 1) ? redO : (board[i][2] == 2) ? yellowO : greenO,
+		(board[i][3] === 0) ? emptyCol : (board[i][3] == 1) ? redO : (board[i][3] == 2) ? yellowO : greenO,
+		(board[i][4] === 0) ? emptyCol : (board[i][4] == 1) ? redO : (board[i][4] == 2) ? yellowO : greenO,
+		(board[i][5] === 0) ? emptyCol : (board[i][5] == 1) ? redO : (board[i][5] == 2) ? yellowO : greenO,
+		(board[i][6] === 0) ? emptyCol : (board[i][6] == 1) ? redO : (board[i][6] == 2) ? yellowO : greenO,
+		(board[i][7] === 0) ? emptyCol : (board[i][7] == 1) ? redO : (board[i][7] == 2) ? yellowO : greenO,		
+		(board[i][8] === 0) ? emptyCol : (board[i][8] == 1) ? redO : (board[i][8] == 2) ? yellowO : greenO);
+	}
+	
+	console.log('===========================');
+	console.log('|        \x1b[36mCONNECT 5\x1b[34m        |');
+	console.log('===========================\x1b[0m');
+}
+
+function resetBoard(board) {
+	for (var row = 0; row < ROWS; row++) {
+		for (var col = 0; col < COLS; col++) {
+			board[row][col] = 0;			
+			cols[col][row].style.backgroundImage="";	// Reset coloured cells
+		}
+	}
+}
+
+// Highlight the winning row
+function show5InARow() {	
+	for (var row = 0; row < ROWS; row++) {
+		for (var col = 0; col < COLS; col++) {
+			winBoard[row][col] = board[row][col];
+		}
+	}	
+	for (var i = 0; i < (CONNECT*2); i += 2) {
+		winBoard[fiveInARow[i]][fiveInARow[i + 1]] = 3; // Highlight winning line
+	}	
+}
+
+function changePlayer() {
+	currentPlayer = (currentPlayer == PLAYER_1) ? PLAYER_2 : PLAYER_1;
+	//displayCurrentPlayer();
+}
--- static/game.js
+++ static/game.js
@@ -4,18 +4,18 @@
 	Connect 5
 	5 in a row game with client side logic
 */
-//var socket = io();
+var socket = io();
 
-const CONNECT = 5;
+//const CONNECT = 5;
 const ROWS = 6;
 const COLS = 9;
 const PLAYER_1 = 1;
 const PLAYER_2 = 2;
 
-var currentPlayer;
-var fiveInARow;
-var ready;
-var gameOver;
+//var currentPlayer;
+//var fiveInARow;
+//var ready;
+//var gameOver;
 var board = [[0,0,0,0,0,0,0,0,0],	// init board
 			 [0,0,0,0,0,0,0,0,0],
 			 [0,0,0,0,0,0,0,0,0],
@@ -23,6 +23,7 @@ var board = [[0,0,0,0,0,0,0,0,0],	// init board
 			 [0,0,0,0,0,0,0,0,0],
 			 [0,0,0,0,0,0,0,0,0]];			
 var winBoard;
+var player, game;
 
 const columns = document.querySelectorAll('.column');
 const col0 = document.querySelectorAll('.col0');	// IDs of cells in each column
@@ -34,9 +35,169 @@ const col5 = document.querySelectorAll('.col5');
 const col6 = document.querySelectorAll('.col6');
 const col7 = document.querySelectorAll('.col7');
 const col8 = document.querySelectorAll('.col8');
+const cols = [col0, col1, col2, col3, col4, col5, col6, col7, col8];
 
-var cols = [col0, col1, col2, col3, col4, col5, col6, col7, col8];
+document.querySelector('#newGameBtn').addEventListener('click', username1Button, false);
+document.querySelector('#existingBtn').addEventListener('click', username2Button, false);
 
+class Player {
+	constructor(name, type) {
+		this.name = name;
+		this.type = type;
+		this.turn = false;
+		this.playsArr = 0;
+	}
+}
+
+class Game {
+	constructor(gameID) {
+		this.gameID = gameID;
+		this.board = board;
+		this.gameOver = false;		
+		this.winBoard = board;
+		this.currentPlayer = 0;
+	}
+	
+	drawBoard(board) {	 
+		for (var row = 0; row < ROWS; row++) {
+			for (var col = 0; col < COLS; col++) {	
+				if (board[row][col] == 0)
+					cols[col][row].style.backgroundImage="linear-gradient(to bottom right, white, grey))";	// empty		
+				else if (board[row][col] == 1)
+					cols[col][row].style.backgroundImage="linear-gradient(to bottom right, red, darkred)";	// player 1
+				else if (board[row][col] == 2)
+					cols[col][row].style.backgroundImage="linear-gradient(to bottom right, yellow, orange)";// player 2
+				else if (board[row][col] == 3)
+					cols[col][row].style.backgroundImage="linear-gradient(to bottom right, lime, green)";	// winning line
+			}
+		}
+	}
+	
+	init() {
+		console.log('\x1b[36mStart Game!!! ' + gameID);
+		this.drawBoard(board);						// Reset: Draw the reset board
+		this.displayCurrentPlayer();				// Reset: Clear the win message
+				
+		if (columns && !this.gameOver) {
+			for (var i = 0; i < columns.length; i++) {
+				columns[i].addEventListener('click', handleClicks, false);
+			}	
+		}
+	}	
+	
+	turn(colID) {
+		//if (player.type == currentPlayer)
+			console.log('Your Turn - column: ' + colID);
+		//else
+		//	console.log('Not Your Turn Yet - column: ' + colID);
+		
+		if (player.turn) {
+			socket.emit('takeTurn', {
+				column: colID,
+				gameID: this.gameID
+			});
+		}
+		
+		this.drawBoard(game.board);
+		player.turn = false;
+	}
+	
+	displayCurrentPlayer() {
+		document.getElementById("player_info").innerText = (this.currentPlayer === 0) ? "Waiting on Player 2 to join" : (this.currentPlayer==PLAYER_1) ? "Player 1 Go" : "Player 2 Go";
+		document.getElementById("player_info").style.color = (this.currentPlayer==PLAYER_1) ? "red" : "orange";
+	}
+
+	showGame(data) {
+		document.getElementById("selectGame").style.display = "none";
+		document.getElementById("gameBoard").style.display = "inline";	
+		document.getElementById("gameBoard").style.display = "inline";	
+		document.getElementById("username").innerText = "Player " + player.type + ": " + data.name;
+		document.getElementById("displayMessage").innerText = "Welcome "+ data.name + ". Player 2 must enter Game ID: \"" + data.gameID + "\" to join";
+	}
+}
+	
+socket.on('newGame', (data) => {
+	game = new Game(data.gameID);
+	game.init();	
+	game.showGame({name: data.username, gameID: data.gameID});
+});
+
+socket.on('player1', (data) => {
+	//const message = `Hello, ${player.getPlayerName()}`;
+	//$('#userHello').html(message);
+	//player.turn = true;
+});
+
+socket.on('player2', (data) => {
+	console.log('player 2 function');
+    //const message = `Hello, ${data.username}`;
+    // Create game for player 2
+    game = new Game(data.gameID);
+	game.init();
+	game.showGame({name: data.username, gameID: data.gameID});
+	player.turn = false;
+	game.currentPlayer = PLAYER_1;
+});
+
+socket.on('turnPlayed', (data) => {
+	console.log('turn played & board updated');
+	
+	console.log(data.board);
+	game.board = data.board;
+	game.drawBoard(data.board);
+	game.currentPlayer = data.player;
+	console.log('Current Player Now: ' + data.player);
+
+	//const opponentType = player.type === PLAYER_1 ? PLAYER_2 : PLAYER_1;
+	//game.updateBoard(opponentType, row, col, data.tile);
+	player.turn = true;
+});
+
+// If the other player wins, this event is received. Notify user game has ended.
+socket.on('gameEnd', (data) => {
+	game.endGame(data.message);
+	socket.leave(data.room);
+});
+
+
+function handleClicks(col) {
+	if (!game.gameOver) {
+		//console.log('Player %d column selected: %s', currentPlayer, col.currentTarget.id);
+		//turn(col.currentTarget.id, currentPlayer);	
+		game.turn(col.currentTarget.id);	
+		
+		//socket.emit('col', {column : col.currentTarget.id, player: currentPlayer})
+		socket.emit('col', {column : col.currentTarget.id, player: player.type})
+	}
+}
+
+function username1Button() {
+	var username1 = document.getElementById("username1").value;
+	
+	if (!username1) {
+	  alert('Please Enter A Username To Play');
+	  return;
+	}
+	
+	socket.emit('create', { username: username1 });
+	player = new Player(username1, PLAYER_1);
+}
+
+function username2Button() {
+	var username2 = document.getElementById("username2").value;
+	var gameIDField = document.getElementById("gameID").value;
+	
+	if (!username2 || !gameIDField) {
+	  alert('Please Enter BOTH Username And Game ID');
+	  return;
+	}
+	
+	console.log('Client (username2Button) - Username: ' + username2 + ' Game ID: ' + gameIDField);	
+	socket.emit('player2join', { username: username2, gameID: gameIDField });
+	player = new Player(username2, PLAYER_2);
+	game.currentPlayer = PLAYER_1;
+}
+/*
 if (columns && !gameOver) {
 	for (var i = 0; i < columns.length; i++) {
 		columns[i].addEventListener('click', turnClick, false);
@@ -66,21 +227,6 @@ function turnClick(col) {
 function turn(colID, player) {
 	checkCol(colID);
 	drawBoard();
-}
-
-function drawBoard() {	 
-	for (var row = 0; row < ROWS; row++) {
-		for (var col = 0; col < COLS; col++) {	
-			if (board[row][col] === 0)
-				cols[col][row].style.backgroundImage="linear-gradient(to bottom right, white, grey))";	// empty		
-			else if (board[row][col] == 1)
-				cols[col][row].style.backgroundImage="linear-gradient(to bottom right, red, darkred)";	// player 1
-			else if (board[row][col] == 2)
-				cols[col][row].style.backgroundImage="linear-gradient(to bottom right, yellow, orange)";// player 2
-			else if (board[row][col] == 3)
-				cols[col][row].style.backgroundImage="linear-gradient(to bottom right, lime, green)";	// winning line
-		}
-	}
 }
 
 function checkWin(player) {
@@ -197,3 +343,4 @@ function resetBoard(board) {
 		}
 	}
 }
+*/
