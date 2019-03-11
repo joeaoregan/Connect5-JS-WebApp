75897f34c6c2a1c9df48623a8d1a37e68ce30611
Joe O'Regan <joeaoregan@yahoo.ie>
2019-03-11 22:49:34 +0000

    Add changes from Chat App, update index page and css

--- static/chat.js
+++ static/chat.js
@@ -1,19 +1,49 @@
 //var username = "Player 2";
 
+var notifySound = new sound("MessageAlert.mp3");
+
 //var socket = io({transports: ['websocket'], upgrade: false});
 var addLi = (message, user) => {		
-	var li = document.createElement('p');
-	li.classList.add('message');
+	//var li = document.createElement('p');
+	//li.classList.add('message');
 	
+	/*
 	if (user) {
 		li.classList.add('right');
 	} else {
 		li.classList.add('left');
 	}
+	*/
 	
-	li.appendChild(document.createTextNode(message));
-	var list = document.getElementById('msgList');
-	list.insertBefore(li, list.childNodes[0]);
+	var outerMessageDiv = document.createElement('div');
+	var newMessageDiv = document.createElement('div');
+	
+	if (user) {
+		outerMessageDiv.classList.add('right-outer');
+		newMessageDiv.classList.add('chat');
+		newMessageDiv.classList.add('right-inner');
+	} else {				
+		//console.log('set class left');
+		outerMessageDiv.classList.add('left-outer');
+		newMessageDiv.classList.add('chat');
+		newMessageDiv.classList.add('left-inner');
+	} 	
+	
+	newMessageDiv.appendChild(document.createTextNode(message));
+	
+	outerMessageDiv.appendChild(newMessageDiv);
+	
+	
+	//li.appendChild(document.createTextNode(message));
+	//var list = document.getElementById('msgList');
+	//list.insertBefore(li, list.childNodes[0]);
+	
+	var messageList = document.getElementById('msgList');
+	messageList.appendChild(outerMessageDiv);	
+	
+	//var scrolldiv = document.getElementById("chatdiv");
+	var scrolldiv = document.getElementById("msgList");
+	scrolldiv.scrollTop = scrolldiv.scrollHeight;
 };
 
 // send value from input box
@@ -33,5 +63,9 @@ document.getElementById("messagetext")
 	}
 });
 
-socket.on('message', (data) => addLi(data.msg, false));
+socket.on('message', (data) => {
+	addLi(data.msg, false);
+	notifySound.play();
+});
+
 //socket.on('user.events', (message) => addLi(message, false));
