7bbd09cda52116a17323abef5248aeb05309626a
Joe O'Regan <joeaoregan@yahoo.ie>
2019-12-19 00:43:35 +0000

    fix win reset, refactor client

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
 */
 const express = require('express'),
 	http = require('http');
-	socketio = require('socket.io');
+socketio = require('socket.io');
 
 var port = process.env.PORT || 3000;
 var app = express();
@@ -22,7 +22,7 @@ var games = [];									// Array of games
 
 class Game {
 	constructor(gameID) {
-		//this.gameID = gameID;
+		this.gameID = gameID;
 		this.currentPlayer = PLAYER_1;
 		this.fiveInARow = Array(10).fill(0);
 		this.gameOver = false;
@@ -30,22 +30,22 @@ class Game {
 		this.player1 = "";
 		this.player2 = "";
 	}
-	
+
 	getBoard() { return this.board; }
 	setBoard(b) { this.board = b; }
 	getGameOver() { return this.gameOver; }
 	setGameOver(t) { this.gameOver = t; }
 	getCurrentPlayer() { return this.currentPlayer; }
 	setCurrentPlayer(p) { this.currentPlayer = p; }
-	getPlayer1Name() {return this.player1; }
-	getPlayer2Name() {return this.player2; }
+	getPlayer1Name() { return this.player1; }
+	getPlayer2Name() { return this.player2; }
 	setPlayer1Name(p1) { this.player1 = p1; }
 	setPlayer2Name(p2) { this.player2 = p2; }
 	get5InARow() { return this.fiveInARow; }
-	set5InARow(f){ this.fiveInARow = f; }
-	
-	resetGame() {
-		this.currentPlayer = PLAYER_1;
+	set5InARow(f) { this.fiveInARow = f; }
+
+	resetGame(movesFirst) {
+		this.currentPlayer = movesFirst;
 		this.fiveInARow = Array(10).fill(0);
 		this.gameOver = false;
 		this.board = Array(6).fill().map(() => Array(9).fill(0));
@@ -55,47 +55,44 @@ class Game {
 io.on('connection', (socket) => {
 	console.log("New Connection");
 	//socket.broadcast.emit('user.events', 'Player 2' + ' is online!');
-    //socket.broadcast.to(data.gameID).emit('user.events', 'Player 2' + ' is online!');
-		
-    socket.on('create', (data) => {
-        socket.join(`game-${++gamesPlayed}`);															// subscribe to a specific channel (game), join a room
- 		games[gamesPlayed] = new Game(gamesPlayed);														// Create a game with an increment of the number of games
- 		games[gamesPlayed].setPlayer1Name(data.username);												// Set Player 1 username				
+	//socket.broadcast.to(data.gameID).emit('user.events', 'Player 2' + ' is online!');
+
+	socket.on('create', (data) => {
+		socket.join(`game-${++gamesPlayed}`);															// subscribe to a specific channel (game), join a room
+		games[gamesPlayed] = new Game(gamesPlayed);														// Create a game with an increment of the number of games
+		games[gamesPlayed].setPlayer1Name(data.username);												// Set Player 1 username				
 		console.log('player 1 name set ');
-						
+
 		socket.emit('newGame', { username: data.username, gameID: `game-${gamesPlayed}` });
-		console.log('Game-'+gamesPlayed+' created by player 1: '+data.username+'. Waiting on Player 2');// Log on server
-    });
-	
-    socket.on('player2join', function (data) {
-        var game = io.nsps['/'].adapter.rooms[data.gameID];
+		console.log('Game-' + gamesPlayed + ' created by player 1: ' + data.username + '. Waiting on Player 2');// Log on server
+	});
+
+	socket.on('player2join', function (data) {
+		var game = io.nsps['/'].adapter.rooms[data.gameID];
 		//console.log('player2join gameID ' + data.gameID + " username " + data.username);
-		
-        if (game && game.length === 1) {
+
+		if (game && game.length === 1) {
 			console.log("player2join: Player 2: " + data.username + " has joined " + data.gameID);
-            socket.join(data.gameID);
-			
+			socket.join(data.gameID);
+
 			games[parseInt(data.gameID.split('-')[1])].setPlayer2Name(data.username);					// Set Player 2 username
 			games[parseInt(data.gameID.split('-')[1])].setCurrentPlayer(PLAYER_1);						// Set the first turn to player 1
-			
-            //socket.broadcast.to(data.gameID).emit('player1', {});
-            socket.broadcast.to(data.gameID).emit('player1', {usernameP2: data.username});
-            //socket.emit('player2', { username: data.username, gameID: data.gameID });					// player 2 username
-            socket.emit('player2', { username: data.username, usernameP1: games[parseInt(data.gameID.split('-')[1])].getPlayer1Name(), gameID: data.gameID });	// SEND: player 2 username, player 1 username, gameID
-        } else {
-            socket.emit('err', { message: 'This game is already full' });
-        }
-    });
-	
+
+			//socket.broadcast.to(data.gameID).emit('player1', {});
+			socket.broadcast.to(data.gameID).emit('player1', { usernameP2: data.username });			// init player 1
+			//socket.emit('player2', { username: data.username, gameID: data.gameID });					// player 2 username
+			socket.emit('player2', { username: data.username, usernameP1: games[parseInt(data.gameID.split('-')[1])].getPlayer1Name(), gameID: data.gameID });	// SEND: player 2 username, player 1 username, gameID
+		} else {
+			socket.emit('err', { message: 'This game is already full' });
+		}
+	});
+
 	socket.on('col', (data) => {
 		index = parseInt(data.gameID.split('-')[1]);
-		
+
 		if (data.player === games[index].getCurrentPlayer()) {
-			//if (checkCol(data.column, index)) {														// check for each room
 			var goodMove = checkCol(data.column, index, data.gameID);									// check for each room
-			//checkWin(games[index].getCurrentPlayer(), parseInt(data.gameID.split('-')[1]));					
-			//changePlayer(index); 																		// Move complete, change the active player
-						
+
 			if (goodMove) {
 				io.to(data.gameID).emit((games[index].getGameOver()) ? 'gameWon' : 'turnPlayed', {
 					board: games[index].getBoard(),
@@ -106,42 +103,39 @@ io.on('connection', (socket) => {
 			}
 		} else {
 			console.log("Not Your Turn Player %s!", data.player);
-			goodMove = false;			
+			goodMove = false;
 			/* NOTIFY THE PLAYER IT IS NOT THEIR TURN --- HTML */
 		}
-		
-		if (!goodMove) {			
+
+		if (!goodMove) {
 			io.to(data.gameID).emit('badMove', {
-				column: data.column, 
-				gameID: data.gameID, 
-				player: games[index].getCurrentPlayer()				
+				column: data.column,
+				gameID: data.gameID,
+				player: games[index].getCurrentPlayer()
 			});
 		}
 	});
-
-    socket.on('gameOver', (data) => {
-        socket.broadcast.to(data.gameID).emit('gameEnd', data);
-    });
-	
+/*
+	socket.on('gameOver', (data) => {
+		socket.broadcast.to(data.gameID).emit('gameEnd', data);
+	});
+*/
 	socket.on('resetGame', (data) => {
 		console.log('\x1b[31m*** ' + data.gameID.toUpperCase() + ' RESET ***\x1b[0m');
-		console.log('Game: '+data.gameID+' Reset by: '+data.player);
-		games[parseInt(data.gameID.split('-')[1])].resetGame();
-        socket.broadcast.to(data.gameID).emit('clearBoard', {player: data.player, gameID: data.gameID});		
+		console.log('Game: ' + data.gameID + ' Reset by: ' + data.player);
+		games[parseInt(data.gameID.split('-')[1])].resetGame(data.winner);
+		socket.broadcast.to(data.gameID).emit('clearBoard', { player: data.player, gameID: data.gameID, winner: data.winner });
 	});
-	
+
 	socket.on('leaveGame', (data) => {
 		console.log('Player : ' + data.player + ' has left Game: ' + data.gameID);
-        socket.broadcast.to(data.gameID).emit('shutdownMsg', data);
+		socket.broadcast.to(data.gameID).emit('shutdownMsg', data);
 	});
-	
-	socket.on('disconnect', function() {
+
+	socket.on('disconnect', function () {
 		console.log('Player  has disconnected');
 	});
-	
-	/*
-		Chat
-	*/
+
 	socket.on('message', (data) => {
 		console.log('MSG - ' + data.gameID + ' ' + data.username + ': ' + data.msg);
 		//socket.broadcast.emit('message', message);	// broadcast to everyone except this
@@ -152,18 +146,17 @@ io.on('connection', (socket) => {
 server.listen(port);
 console.clear();																					// Clear console text
 console.log("Server running at http://localhost: " + port);
-displayBoard(Array(6).fill().map(() => Array(9).fill(0)),"");										// Show empty board to begin with
-
+displayBoard(Array(6).fill().map(() => Array(9).fill(0)), "");										// Show empty board to begin with
 
 function checkCol(col, index, gameID) {
 	var cb = games[index].getBoard();		// get the board
-	console.log('game-'+index);
-	console.log('Check Column '+(col+1)+' For Player '  + games[index].getCurrentPlayer());			// show column number as displayed in console
-	
+	console.log('game-' + index);
+	console.log('Check Column ' + (col + 1) + ' For Player ' + games[index].getCurrentPlayer());			// show column number as displayed in console
+
 	if (cb[0][col] != 0) {
 		console.log('\x1b[31mError:\x1b[0m Column %s is full!', col);
-	} else {	
-		for (var i = ROWS-1; i >= 0; i--) {
+	} else {
+		for (var i = ROWS - 1; i >= 0; i--) {
 			if (cb[i][col] == 0) {
 				cb[i][col] = games[index].getCurrentPlayer();
 				break;
@@ -172,40 +165,40 @@ function checkCol(col, index, gameID) {
 		games[index].setBoard(cb);			// store the board
 		displayBoard(cb, index);
 		cb = null;
-		
-		checkWin(games[index].getCurrentPlayer(), parseInt(gameID.split('-')[1]));					
+
+		checkWin(games[index].getCurrentPlayer(), parseInt(gameID.split('-')[1]));
 		changePlayer(index); 																		// Move complete, change the active player
 		return true;
 	}
-	
+
 	return false;	// column is full
 }
 
 function checkWin(player, index) {
 	var board = games[index].getBoard();
-	
+
 	// Diagonals
 	var win = false;
-	
+
 	for (var row = 0; row <= ROWS - CONNECT; row++) {
 		// up to right
-		for (var col = CONNECT-1; col < COLS; col++) {
+		for (var col = CONNECT - 1; col < COLS; col++) {
 			if (board[row][col] == player && board[row + 1][col - 1] == player && board[row + 2][col - 2] == player
-					&& board[row + 3][col - 3] == player && board[row + 4][col - 4] == player) {
+				&& board[row + 3][col - 3] == player && board[row + 4][col - 4] == player) {
 				win = true;
 				//games[index].set5InARow(new Array(row,col,row+1,col-1,row+2,col-2,row+3,col-3,row+4,col-4)); // Highlight winning move
-				games[index].set5InARow([row,col,row+1,col-1,row+2,col-2,row+3,col-3,row+4,col-4]); // Highlight winning move
+				games[index].set5InARow([row, col, row + 1, col - 1, row + 2, col - 2, row + 3, col - 3, row + 4, col - 4]); // Highlight winning move
 				break;
 			}
 		}
-		
+
 		// up to left
 		if (!win) {
 			for (var col = 0; col <= COLS - CONNECT; col++) {
 				if (board[row][col] == player && board[row + 1][col + 1] == player && board[row + 2][col + 2] == player
-						&& board[row + 3][col + 3] == player && board[row + 4][col + 4] == player) {
+					&& board[row + 3][col + 3] == player && board[row + 4][col + 4] == player) {
 					win = true;
-					games[index].set5InARow([row,col,row+1,col+1,row+2,col+2,row+3,col+3,row+4,col+4]);
+					games[index].set5InARow([row, col, row + 1, col + 1, row + 2, col + 2, row + 3, col + 3, row + 4, col + 4]);
 					break;
 				}
 			}
@@ -218,9 +211,9 @@ function checkWin(player, index) {
 	for (var row = 0; row < ROWS; row++) {
 		for (var col = 0; col <= COLS - CONNECT; col++) {
 			if (board[row][col] == player && board[row][col + 1] == player && board[row][col + 2] == player
-					&& board[row][col + 3] == player && board[row][col + 4] == player) {
+				&& board[row][col + 3] == player && board[row][col + 4] == player) {
 				win = true;
-				games[index].set5InARow([row,col,row,col+1,row,col+2,row,col+3,row,col+4]);
+				games[index].set5InARow([row, col, row, col + 1, row, col + 2, row, col + 3, row, col + 4]);
 				break;
 			}
 		}
@@ -231,15 +224,15 @@ function checkWin(player, index) {
 	for (var row = 0; row <= ROWS - CONNECT; row++) {
 		for (var col = 0; col < COLS; col++) {
 			if (board[row][col] == player && board[row + 1][col] == player && board[row + 2][col] == player
-					&& board[row + 3][col] == player && board[row + 4][col] == player) {
+				&& board[row + 3][col] == player && board[row + 4][col] == player) {
 				win = true;
-				games[index].set5InARow([row,col,row+1,col,row+2,col,row+3,col,row+4,col]);
+				games[index].set5InARow([row, col, row + 1, col, row + 2, col, row + 3, col, row + 4, col]);
 				break;
 			}
 		}
-		if (win) break; 
+		if (win) break;
 	}
-	
+
 	if (win) {
 		games[index].setGameOver(true);
 		highlightWinner(player, board);
@@ -254,32 +247,32 @@ function displayBoard(board, game) {
 		console.log('\n  \x1b[36m%s\x1b[0m %s', 'Game: ', game);  //cyan
 	}
 	console.log('\x1b[34m_\x1b[0m1\x1b[34m__\x1b[0m2\x1b[34m__\x1b[0m3\x1b[34m__\x1b[0m4\x1b[34m__\x1b[0m5\x1b[34m__\x1b[0m6\x1b[34m__\x1b[0m7\x1b[34m__\x1b[0m8\x1b[34m__\x1b[0m9\x1b[34m_');
-	
+
 	for (var i = 0; i < ROWS; i++) { // display 0 to 5
 		for (var j = 0; j < 9; j++) {
 			process.stdout.write((b[i][j] === 0) ? '[\x1b[34m ]' : (b[i][j] == 1) ? '[\x1b[31mO\x1b[34m]' : (b[i][j] == 2) ? '[\x1b[33mO\x1b[34m]' : '[\x1b[32mO\x1b[34m]');
 		}
 		process.stdout.write('\n');
-	}	
-	
+	}
+
 	console.log("=".repeat(27));
 	console.log('|        \x1b[36mCONNECT 5\x1b[34m        |');
-	console.log("=".repeat(27)+"\x1b[0m");
+	console.log("=".repeat(27) + "\x1b[0m");
 	b = null;
 }
 
 // Highlight the winning row
 function highlightWinner(player, finalBoard) {
-	console.log("\n\x1b[32m"+"*".repeat(27) + '\n* Player '+player+' Is The Winner! *\n' + "*".repeat(27)+ "\x1b[0m");		
+	console.log("\n\x1b[32m" + "*".repeat(27) + '\n* Player ' + player + ' Is The Winner! *\n' + "*".repeat(27) + "\x1b[0m");
 	var winBoard = finalBoard;
-	
-	for (var i = 0; i < (CONNECT*2); i += 2) {		
+
+	for (var i = 0; i < (CONNECT * 2); i += 2) {
 		winBoard[games[index].get5InARow()[i]][games[index].get5InARow()[i + 1]] = 3; // Highlight winning line
 	}
-	
+
 	displayBoard(winBoard, 'over');
 }
 
-function changePlayer(index) {	
+function changePlayer(index) {
 	games[index].setCurrentPlayer((games[index].getCurrentPlayer() == PLAYER_1 && !games[index].getGameOver()) ? PLAYER_2 : PLAYER_1);	// If the game is not over change the current player, otherwise leave as is for winner message
 }
--- static/game.js
+++ static/game.js
@@ -4,63 +4,45 @@
 	Connect 5 - Multiplayer
 */
 const socket = io();
-const ROWS = 6, COLS = 9;
-const PLAYER_1 = 1, PLAYER_2 = 2;
-
-var board = Array(6).fill().map(() => Array(9).fill(0));
-var winBoard;
-var player, game;
-
-var badMoveSnd, gameOverSnd;
-
+const ROWS = 6, COLS = 9, PLAYER_1 = 1, PLAYER_2 = 2, WIN = 3;
+const badMoveSnd = new sound("audio/BadMove.mp3"); // Sound when not the players turn
+const gameOverSnd = new sound("audio/Win.mp3"); // Sound when game won
 const columns = document.querySelectorAll('.column');
+
+var board, winBoard, player, game;
 var cols = [];
 for (var i = 0; i < 9; i++) {
-	cols[i] = document.querySelectorAll('.col' + i);	// Used to draw the disks
+	cols[i] = document.querySelectorAll('.col' + i); // Used to draw the disks
 }
 document.querySelector('#newGameBtn').addEventListener('click', username1Button, false);
 document.querySelector('#existingBtn').addEventListener('click', username2Button, false);
 
 class Player {
 	constructor(name, type) {
-		this.name = name;			// Players name
-		this.opponent = "";			// Opponents name
-		this.type = type;			// Player 1 or 2
-		this.turn = false;			// Players can only choose a column if it is there turn
+		this.name = name;		// Players name
+		this.opponent = "";		// Opponents name
+		this.type = type;		// Player 1 or 2
+		this.turn = false;		// Players can only choose a column if it is there turn
 	}
 }
 
 class Game {
 	constructor(gameID) {
 		this.gameID = gameID;
-		this.board = board;
-		this.winBoard = board;
-		this.gameOver = false;
 		this.currentPlayer = 0;
+		this.winner = 0;
 	}
 
-	getCurrentPlayer() { return this.currentPlayer; }
-	setCurrentPlayer(player) { this.currentPlayer = player; }
-	getGameID() { return this.gameID; }
-	setGameOver(gameOver) { this.gameOver = gameOver; }
-	getBoard() { return this.board; }
-
-	setup(gameID) {
-		this.gameID = gameID;
-		this.board = Array(ROWS).fill().map(() => Array(COLS).fill(0));
+	init(movesFirst) {
+		this.finished = false; // The game is not over
+		this.currentPlayer = movesFirst;
+		this.board = Array(ROWS).fill().map(() => Array(COLS).fill(0)); // Set/Reset the board
 		this.winBoard = board;
-		this.gameOver = false;
-		this.currentPlayer = PLAYER_1;
-	}
-
-	init() {
-		badMoveSnd = new sound("audio/BadMove.mp3");
-		gameOverSnd = new sound("audio/Win.mp3");
 		clearColumnBG();
 		console.log('\x1b[36mStart Game!!! ' + this.gameID);
-		this.drawBoard(this.board);													// Reset: Draw the reset board
+		this.drawBoard(this.board); // Reset: Draw the reset board
 
-		if (columns && !this.gameOver) {
+		if (columns && !this.finished) {
 			for (var i = 0; i < columns.length; i++) {
 				columns[i].addEventListener('click', handleClicks, false);
 			}
@@ -70,62 +52,47 @@ class Game {
 	turn(colID) {
 		if (player.type == this.currentPlayer) {
 			if (player.turn) {
-				socket.emit('takeTurn', {
-					column: colID,
-					gameID: this.gameID
-				});
-
-				console.log('Your Turn - column: ' + colID);
-				player.turn = false;
+				socket.emit('takeTurn', { column: colID, gameID: this.gameID });
+				player.turn = false; // console.log('Your Turn - column: ' + colID);
 			}
-
-			this.drawBoard(this.board);
 		}
 	}
 
 	drawBoard(board) {
+		console.log('*** updating board ***')
 		for (var row = 0; row < ROWS; row++) {
 			for (var col = 0; col < COLS; col++) {
-				if (board[row][col] == 0)
-					cols[col][row].style.backgroundImage = "linear-gradient(to bottom right, white, grey))";	// empty		
-				else if (board[row][col] == 1)
-					cols[col][row].style.backgroundImage = "linear-gradient(to bottom right, red, darkred)";	// player 1
-				else if (board[row][col] == 2)
-					cols[col][row].style.backgroundImage = "linear-gradient(to bottom right, yellow, orange)";// player 2
-				else if (board[row][col] == 3)
-					cols[col][row].style.backgroundImage = "linear-gradient(to bottom right, lime, green)";	// winning line
+				var diskColour = (board[row][col] == PLAYER_1) ? 'red, darkred' : (board[row][col] == PLAYER_2) ? 'yellow, orange' : (board[row][col] == WIN) ? 'lime, green' : 'white, grey';
+				cols[col][row].style.backgroundImage = "linear-gradient(to bottom right, " + diskColour + ")";
 			}
 		}
 
-		if (!this.gameOver) {
+		if (!this.finished) {
 			var formattedGameID = this.gameID.charAt(0).toUpperCase() + this.gameID.substring(1)
-			document.getElementById("player_info").innerText = (this.currentPlayer === 0) ? "Waiting on Player 2 to join \"" + formattedGameID + "\"" : (this.currentPlayer == PLAYER_1) ? "Player 1 Go" : "Player 2 Go";
+			document.getElementById("player_info").innerText = (this.currentPlayer === 0) ? "Waiting on Player 2 to join \"" + formattedGameID + "\"" :
+				(this.currentPlayer == PLAYER_1) ? "Player 1 Go" : "Player 2 Go";
 		} else {
 			document.getElementById("player_info").innerText = "Player " + this.currentPlayer + " Is The Winner!";
+			this.winner=this.currentPlayer;
 		}
 		document.getElementById("player_info").style.color = (this.currentPlayer == PLAYER_1) ? "red" : "orange";
 	}
 
 	showDetails(data) {
-		document.getElementById("selectGame").style.display = "none";
-		document.getElementById("gameBoard").style.display = "inline";
-		document.getElementById("hidechat").style.display = "inline";
+		document.getElementById("selectGame").style.display = "none"; // Hide the game start screen
+		document.getElementById("gameBoard").style.display = "inline"; // Show the game board
+		document.getElementById("hidechat").style.display = "inline"; // Show the chat window
 		document.getElementById("show-gameid").innerText = "Game: " + data.gameID;
 
-		if (game.getCurrentPlayer() === 0) {
+		if (game.currentPlayer === 0) {
 			document.getElementById("username").innerText = "Player " + player.type + ": " + player.name;
-		}
-		if (game.getCurrentPlayer() != 0 && player.type == PLAYER_1) {
-			document.getElementById("username").innerText = "Player 1: " + player.name + " Vs Player 2: " + player.opponent;
-		} else if (game.getCurrentPlayer() != 0 && player.type == PLAYER_2) {
-			document.getElementById("username").innerText = "Player 2: " + player.name + " Vs Player 1: " + player.opponent;
+		} else {
+			this.drawBoard(this.board); // Update info_message
+			console.log('player opponent: ' + player.opponent);
+			document.getElementById("username").innerText = (player.type == PLAYER_1) ? ("Player 1: " + player.name + " Vs Player 2: " + player.opponent) : ("Player 2: " + player.name + " Vs Player 1: " + player.opponent);
 		}
 
-		if (!this.currentPlayer == PLAYER_1) {
-			document.getElementById("displayMessage").innerText = "Welcome " + player.name + ". Player 2 must enter Game ID: \"" + data.gameID + "\" to join";
-		} else {
-			document.getElementById("displayMessage").innerText = "Welcome " + player.name;
-		}
+		document.getElementById("displayMessage").innerText = "Welcome " + player.name + ((!this.currentPlayer == PLAYER_1) ? ". Player 2 must enter Game ID: \"" + data.gameID + "\" to join" : "");
 	}
 
 	addMessage(data) {
@@ -136,49 +103,42 @@ class Game {
 
 socket.on('newGame', (data) => {
 	game = new Game(data.gameID);
-	game.init();
+	game.init(0); // Init game, but player not ready to take turn until opposite player joins
 	game.showDetails({ name: data.username, gameID: data.gameID });
 });
 
 socket.on('player1', (data) => {
-	console.log('Player 1 Init');
-	game.setCurrentPlayer(PLAYER_1);
-	player.opponent = data.usernameP2;								// Set opponent name;
+	game.currentPlayer = PLAYER_1; // Set current player after player 2 joins game
+	console.log('game current player: ' + game.currentPlayer)
+	player.opponent = data.usernameP2; // Set opponent name // console.log('Player 1 Init');
 	player.turn = true;
-	game.showDetails({ name: player.name, gameID: game.getGameID() });
-	game.drawBoard(game.getBoard());
+	game.showDetails({ name: player.name, gameID: game.gameID });
 });
 
 // Create game for player 2
 socket.on('player2', (data) => {
-	console.log('Player 2 Init');
-	game = new Game(data.gameID);
-	game.init();
-	game.setCurrentPlayer(PLAYER_1);
-	player.opponent = data.usernameP1;								// Set opponent name;
+	game = new Game(data.gameID); // console.log('Player 2 Init');
+	game.init(PLAYER_1); // Overwrites initial message in showDetails()
+	player.opponent = data.usernameP1; // Set opponent name;
 	game.showDetails({ name: data.username, gameID: data.gameID });
-	game.drawBoard(game.getBoard());
 });
 
 socket.on('turnPlayed', (data) => {
-	console.log('turn played & board updated');
-	game.board = data.board;
-	game.setCurrentPlayer(data.player);
-	console.log('Current Player Now: ' + data.player);
+	game.board = data.board; // console.log('turn played & board updated');
+	game.currentPlayer = data.player; // console.log('Current Player Now: ' + data.player);
 	if (player.type == game.currentPlayer)
-		document.getElementById("column-" + String(data.column)).style.backgroundColor = (player.type == PLAYER_1) ? "yellow" : "red";
-	game.drawBoard(data.board);
+		document.getElementById("column-" + String(data.column)).style.backgroundColor = (player.type == PLAYER_1) ? "yellow" : "red"; // Highlight the opposition players last move
+	game.drawBoard(data.board); // Update the game board
 	player.turn = true;
 });
 
-socket.on('badMove', (data) => {
+socket.on('badMove', () => {
 	badMoveSnd.play();
 });
 
 socket.on('gameWon', (data) => {
 	gameOverSnd.play();
-	console.log('Game Finished');
-	game.setGameOver(true);
+	game.finished = true; // console.log('Game Finished');
 	game.board = data.board;
 	clearColumnBG();
 	game.drawBoard(data.board);
@@ -207,19 +167,21 @@ socket.on('shutdownMsg', (data) => {
 
 socket.on('clearBoard', (data) => {
 	resetGame();  // LOOP
-	game.drawBoard(game.getBoard());
-	game.addMessage({ message: 'Game Reset By ' + data.player + '!!! Player 1 To Restart', colour: 'blue' });
+	game.addMessage({ message: 'Game Reset By ' + data.player + '!!! Player '+ data.winner +' To Restart', colour: 'blue' });
 });
 
 function handleClicks(col) {
 	clearColumnBG();
-	if (!game.gameOver) {
+	if (!game.finished) {
 		if (player.type == game.currentPlayer) {
 			var columnSelected = parseInt(col.currentTarget.id.split('-')[1]);
 			game.turn(columnSelected);
-			socket.emit('col', { column: columnSelected, player: player.type, gameID: game.getGameID() })
-		} else if (game.getCurrentPlayer() !== 0) {
-			game.addMessage({ message: 'Player ' + ((player.type == PLAYER_1) ? PLAYER_2 : PLAYER_1) + ' Go. Please Wait For Your Turn!', colour: ((player.type == PLAYER_1) ? 'red' : 'orange') });
+			socket.emit('col', { column: columnSelected, player: player.type, gameID: game.gameID })
+		} else if (game.currentPlayer !== 0) {
+			game.addMessage({
+				message: 'Player ' + ((player.type == PLAYER_1) ? PLAYER_2 : PLAYER_1) +
+					' Go. Please Wait For Your Turn!', colour: ((player.type == PLAYER_1) ? 'red' : 'orange')
+			});
 			badMoveSnd.play();
 		} else {
 			game.addMessage({ message: 'Please Wait For Player 2 To Connect!', colour: ((player.type == PLAYER_1) ? 'red' : 'orange') });
@@ -233,29 +195,16 @@ function clearColumnBG() {
 	}
 }
 
-function startGame() {
-	console.log('Start New Game');
-	game = new Game(game.gameID);
-	game.init();
-}
-
-/*
-	When reset button is pressed
-*/
+// When reset button is pressed
 function resetGame() {
 	console.log('*** GAME RESET ***');
-	for (var row = 0; row < ROWS; row++) {
-		for (var col = 0; col < COLS; col++) {
-			game.board[row][col] = 0;
-			cols[col][row].style.backgroundImage = "";	// Reset coloured cells
-		}
-	}
+	game.init((game.finished)?game.winner:PLAYER_1); // Game resets, Player 1 makes first move
+}
 
-	board = Array(6).fill().map(() => Array(9).fill(0));
-
-	game.init();
-	game.setup(game.gameID);
-	game.drawBoard(game.getBoard());
+function resetGameButton() {
+	console.log('*** resetGameButton() *** winner: '+ game.winner)
+	socket.emit('resetGame', { player: player.name, gameID: game.gameID, winner: game.winner });
+	resetGame();
 }
 
 function username1Button() {
@@ -263,11 +212,10 @@ function username1Button() {
 
 	if (!username1) {
 		alert('Please Enter A Username To Play');
-		return;
+	} else {
+		socket.emit('create', { username: username1 });
+		player = new Player(username1, PLAYER_1);
 	}
-
-	socket.emit('create', { username: username1 });
-	player = new Player(username1, PLAYER_1);
 }
 
 function username2Button() {
@@ -276,20 +224,15 @@ function username2Button() {
 
 	if (!username2 || !gameIDField) {
 		alert('Please Enter BOTH Username And Game ID');
-		return;
+	} else {
+		socket.emit('player2join', { username: username2, gameID: gameIDField });
+		player = new Player(username2, PLAYER_2);
 	}
 
-	socket.emit('player2join', { username: username2, gameID: gameIDField });
-	player = new Player(username2, PLAYER_2);
-}
-
-function resetGameButton() {
-	resetGame();
-	socket.emit('resetGame', { player: player.name, gameID: game.getGameID() });
 }
 
 function leaveGameButton() {
-	socket.emit('leaveGame', { player: player.name, gameID: game.getGameID() });
+	socket.emit('leaveGame', { player: player.name, gameID: game.gameID });
 	location.reload(true);
 }
 
@@ -315,11 +258,6 @@ function sound(src) {
 	this.sound.style.display = "none";
 	document.body.appendChild(this.sound);
 
-	this.play = function () {
-		this.sound.play();
-	}
-
-	this.stop = function () {
-		this.sound.pause();
-	}
+	this.play = function () { this.sound.play(); }
+	this.stop = function () { this.sound.pause(); }
 }
